f353efeb66e42c53dbed1acdda32556f3017933f
Alex Vit <ryujo.la@gmail.com>
2017-05-31 17:15:14 +0300

    feat: Add image grid to main screen

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TEST_POSTER_URL = "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg";
+    private static final int N_COLUMNS = 2;
+
+    private RecyclerView mMovieGridRecyclerView;
+    private MovieGridAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+        loadData();
+    }
+
+    private void loadData() {
+        String[] fakeData = {
+                TEST_POSTER_URL,
+                TEST_POSTER_URL,
+                TEST_POSTER_URL,
+                TEST_POSTER_URL,
+                TEST_POSTER_URL,
+                TEST_POSTER_URL,
+                TEST_POSTER_URL
+        };
+        mAdapter.setMovieData(fakeData);
+    }
+
+    private void initRecyclerView() {
+        mMovieGridRecyclerView = (RecyclerView) findViewById(R.id.movie_grid);
+        mMovieGridRecyclerView.setLayoutManager(new GridLayoutManager(this, N_COLUMNS));
+
+        mAdapter = new MovieGridAdapter();
+        mMovieGridRecyclerView.setAdapter(mAdapter);
+
+        mMovieGridRecyclerView.setHasFixedSize(true);
     }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/MovieGridAdapter.java
@@ -0,0 +1,59 @@
+package com.example.android.popularmovies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by alex on 5/31/2017.
+ */
+
+class MovieGridAdapter extends RecyclerView.Adapter<MovieGridAdapter.MovieGridAdapterViewHolder> {
+
+    private String[] movieData;
+
+    void setMovieData(String[] movieData) {
+        this.movieData = movieData;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public MovieGridAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int itemLayoutId = R.layout.movie_grid_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(itemLayoutId, parent, false);
+        return new MovieGridAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieGridAdapterViewHolder holder, int position) {
+        String posterUrl = movieData[position];
+        Picasso
+                .with(holder.mPosterImageView.getContext())
+                .load(posterUrl)
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(holder.mPosterImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (movieData == null) ? 0 : movieData.length;
+    }
+
+    class MovieGridAdapterViewHolder extends RecyclerView.ViewHolder {
+        final ImageView mPosterImageView;
+
+        MovieGridAdapterViewHolder(View itemView) {
+            super(itemView);
+            mPosterImageView = (ImageView) itemView.findViewById(R.id.poster_image);
+        }
+    }
+}
