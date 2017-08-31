2446c22d7fac0c051d0697a8dbe4e11d91fe01b5
Alex Vit <ryujo.la@gmail.com>
2017-08-31 10:47:18 +0300

    Upgrade Movie model

--- app/src/main/java/com/alexvit/android/popularmovies/data/models/Movie.java
+++ app/src/main/java/com/alexvit/android/popularmovies/data/models/Movie.java
@@ -1,165 +1,96 @@
 package com.alexvit.android.popularmovies.data.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Aleksandrs Vitjukovs on 5/31/2017.
- *
- * GSON and Parcelable implementation generated using
- * - http://www.parcelabler.com/
- * - http://www.jsonschema2pojo.org/
+ * Created by Aleksandrs Vitjukovs on 8/31/2017.
  */
 
-public class Movie implements Parcelable {
+@Entity(tableName = "movies")
+public class Movie {
 
-    @SuppressWarnings("unused")
-    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
-        @Override
-        public Movie createFromParcel(Parcel in) {
-            return new Movie(in);
-        }
-
-        @Override
-        public Movie[] newArray(int size) {
-            return new Movie[size];
-        }
-    };
-    @SerializedName("poster_path")
     @Expose
+    @SerializedName("id")
+    @PrimaryKey
+    public Long id;
+
+    @Expose
+    @SerializedName("poster_path")
+    @ColumnInfo(name = "poster_path")
     public String posterPath;
+
     @SerializedName("adult")
     @Expose
     public Boolean adult;
+
     @SerializedName("overview")
     @Expose
     public String overview;
+
     @SerializedName("release_date")
     @Expose
+    @ColumnInfo(name = "release_date")
     public String releaseDate;
-    @SerializedName("genre_ids")
-    @Expose
-    public List<Integer> genreIds = null;
-    @SerializedName("id")
-    @Expose
-    public Integer id;
+
     @SerializedName("original_title")
     @Expose
+    @ColumnInfo(name = "original_title")
     public String originalTitle;
+
     @SerializedName("original_language")
     @Expose
+    @ColumnInfo(name = "original_language")
     public String originalLanguage;
+
     @SerializedName("title")
     @Expose
     public String title;
+
     @SerializedName("backdrop_path")
     @Expose
+    @ColumnInfo(name = "backdrop_path")
     public String backdropPath;
+
     @SerializedName("popularity")
     @Expose
     public Double popularity;
+
     @SerializedName("vote_count")
     @Expose
+    @ColumnInfo(name = "vote_count")
     public Integer voteCount;
+
     @SerializedName("video")
     @Expose
     public Boolean video;
+
     @SerializedName("vote_average")
     @Expose
+    @ColumnInfo(name = "vote_average")
     public Double voteAverage;
 
-    public boolean isFavorite = false;
+    /*
+     * API-only fields
+     */
 
-    public Movie() {
-    }
+    @SerializedName("genre_ids")
+    @Expose
+    @Ignore
+    public List<Integer> genreIds = null;
 
-    protected Movie(Parcel in) {
-        posterPath = in.readString();
-        byte adultVal = in.readByte();
-        adult = adultVal == 0x02 ? null : adultVal != 0x00;
-        overview = in.readString();
-        releaseDate = in.readString();
-        if (in.readByte() == 0x01) {
-            genreIds = new ArrayList<Integer>();
-            in.readList(genreIds, Integer.class.getClassLoader());
-        } else {
-            genreIds = null;
-        }
-        id = in.readByte() == 0x00 ? null : in.readInt();
-        originalTitle = in.readString();
-        originalLanguage = in.readString();
-        title = in.readString();
-        backdropPath = in.readString();
-        popularity = in.readByte() == 0x00 ? null : in.readDouble();
-        voteCount = in.readByte() == 0x00 ? null : in.readInt();
-        byte videoVal = in.readByte();
-        video = videoVal == 0x02 ? null : videoVal != 0x00;
-        voteAverage = in.readByte() == 0x00 ? null : in.readDouble();
-    }
+    /*
+     * DB-only fields
+     */
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
+    public Boolean favorite;
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(posterPath);
-        if (adult == null) {
-            dest.writeByte((byte) (0x02));
-        } else {
-            dest.writeByte((byte) (adult ? 0x01 : 0x00));
-        }
-        dest.writeString(overview);
-        dest.writeString(releaseDate);
-        if (genreIds == null) {
-            dest.writeByte((byte) (0x00));
-        } else {
-            dest.writeByte((byte) (0x01));
-            dest.writeList(genreIds);
-        }
-        if (id == null) {
-            dest.writeByte((byte) (0x00));
-        } else {
-            dest.writeByte((byte) (0x01));
-            dest.writeInt(id);
-        }
-        dest.writeString(originalTitle);
-        dest.writeString(originalLanguage);
-        dest.writeString(title);
-        dest.writeString(backdropPath);
-        if (popularity == null) {
-            dest.writeByte((byte) (0x00));
-        } else {
-            dest.writeByte((byte) (0x01));
-            dest.writeDouble(popularity);
-        }
-        if (voteCount == null) {
-            dest.writeByte((byte) (0x00));
-        } else {
-            dest.writeByte((byte) (0x01));
-            dest.writeInt(voteCount);
-        }
-        if (video == null) {
-            dest.writeByte((byte) (0x02));
-        } else {
-            dest.writeByte((byte) (video ? 0x01 : 0x00));
-        }
-        if (voteAverage == null) {
-            dest.writeByte((byte) (0x00));
-        } else {
-            dest.writeByte((byte) (0x01));
-            dest.writeDouble(voteAverage);
-        }
-    }
 
-    public String year() {
-        return releaseDate.split("-")[0];
-    }
 }
--- app/src/main/java/com/alexvit/android/popularmovies/data/models/db/Movie.java
+++
@@ -1,8 +0,0 @@
-package com.alexvit.android.popularmovies.data.models.db;
-
-/**
- * Created by Aleksandrs Vitjukovs on 8/31/2017.
- */
-
-public class Movie {
-}
