2b38bcd13f344d9da0794e5b2be2d5431341b079
Alexander Vitjukov <ryujo.la@gmail.com>
2017-09-08 11:38:04 +0300

    Add blank Browse fragment

--- app/src/main/java/com/alexvit/android/popularmovies/tv/TvActivity.java
+++ app/src/main/java/com/alexvit/android/popularmovies/tv/TvActivity.java
@@ -1,10 +1,20 @@
 package com.alexvit.android.popularmovies.tv;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.alexvit.android.popularmovies.R;
 
 /**
  * Created by alexander.vitjukov on 08.09.2017.
  */
 
 public class TvActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv);
+    }
 }
---
+++ app/src/main/java/com/alexvit/android/popularmovies/tv/TvFragment.java
@@ -0,0 +1,10 @@
+package com.alexvit.android.popularmovies.tv;
+
+import android.support.v17.leanback.app.BrowseFragment;
+
+/**
+ * Created by alexander.vitjukov on 08.09.2017.
+ */
+
+public class TvFragment extends BrowseFragment {
+}
