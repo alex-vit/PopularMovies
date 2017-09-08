fd7c0bad7bc22a09d35fd8047408dedc33820a56
Alexander Vitjukov <ryujo.la@gmail.com>
2017-09-08 11:31:59 +0300

    TV dependency and manifest setup

---
+++ app/src/main/java/com/alexvit/android/popularmovies/tv/TvActivity.java
@@ -0,0 +1,10 @@
+package com.alexvit.android.popularmovies.tv;
+
+import android.app.Activity;
+
+/**
+ * Created by alexander.vitjukov on 08.09.2017.
+ */
+
+public class TvActivity extends Activity {
+}
